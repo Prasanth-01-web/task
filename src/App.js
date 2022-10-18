60c7ebdf544cb5311707d20222c7d7ef9eea35dd
Prasanth <114915028+Prasanth-01-web@users.noreply.github.com>
2022-10-18 11:04:00 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import Form from './Components2/InsuranceForm/Form';
+// import Content from './Components/Content/content';
+// import Footer from './Components/Footer/footer';
+// import Headers from './Components/Header/header';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Headers/>
+      <Content/>
+      <Footer/> */}
+      <Form/>
+
+    </div>
+  );
+}
+
+export default App;
